64d10b2191968b0d50297f1b1f74799f9bbd9a48
Pedrito16 <proibidofantasma@gmail.com>
2025-09-30 19:22:02 -0300

    finalizei a seleção da loja, agora falta colocar as informações pra comprar

--- 5Days/Assets/Scripts/LojaScript.cs
+++ 5Days/Assets/Scripts/LojaScript.cs
@@ -7,6 +7,8 @@ public class LojaScript : MonoBehaviour
     [SerializeField] HorizontalLayoutGroup layoutGroup; //to usando Layout que é mais facil de organizar e mexer tipo slider
     [SerializeField] RectTransform[] items;
     [SerializeField] RectTransform selection;
+    [SerializeField] Vector2 selectionClamp;
+    [Header("Read-Only")]
     [SerializeField] int currentSelected;
     [SerializeField] int fieldToMove;
     void Start()
@@ -18,14 +20,15 @@ public class LojaScript : MonoBehaviour
         float horizontal = Input.GetAxisRaw("Horizontal");
         if(Input.GetButtonDown("Horizontal"))
         {
+            print("antes do horizontal");
             if(!CheckIfCanMoveSelection((int)horizontal))
-            {//
+            {
                 print("não pode mover mais");
                 return;
             }
+            SelectOther(new Vector2(horizontal, 0));
             if (fieldToMove == 2 || fieldToMove == -2)
                 Mexeu(new Vector2(horizontal, 0));
-            SelectOther(new Vector2(horizontal, 0));
         }
     }
     void Mexeu(Vector2 direction)
@@ -33,7 +36,7 @@ public class LojaScript : MonoBehaviour
         direction = -direction; //inverte o vetor, pra ficar certinho
         for(int i = 0; i < items.Length; i++)
         {
-            fieldToMove = -1;
+            fieldToMove = (int)direction.x * -1;
             items[i].anchoredPosition += direction * 160;
         }
     }
@@ -43,17 +46,21 @@ public class LojaScript : MonoBehaviour
         currentSelected += horizontal;
         fieldToMove += horizontal;
         currentSelected = Mathf.Clamp(currentSelected, 0, items.Length - 1);
-        StartCoroutine(MexerSelect(horizontal, 0.75f));
+        StartCoroutine(MexerSelect(horizontal, 0.2f));
     }
     IEnumerator MexerSelect(int sentido, float duration)
     {
         float iterador = 0;
-        Vector2 selecionPos = new Vector2(selection.anchoredPosition.x, selection.anchoredPosition.y);
-        Vector2 newPos = selecionPos + new Vector2(sentido * 54, 0);
+        Vector2 selectionPos = new Vector2(selection.anchoredPosition.x, selection.anchoredPosition.y);
+        Vector2 newPos = selectionPos + new Vector2(sentido * 53
+            , 0);
         while (iterador < duration)
         {
-            selection.anchoredPosition = Vector2.Lerp(selection.anchoredPosition, newPos, iterador / duration);
+            selectionPos = Vector2.Lerp(selection.anchoredPosition, newPos, iterador / duration);
+            selectionPos.x = Mathf.Clamp(selectionPos.x, selectionClamp.x, selectionClamp.y);
+            selection.anchoredPosition = selectionPos;
             iterador += Time.deltaTime;
+            
             yield return null;
         }
     }
